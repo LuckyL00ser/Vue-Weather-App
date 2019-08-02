209c7f4cc81122946e5871431cb0cf3cdaee37e8
Karol Wiater <karolwiater0@gmail.com>
2019-08-02 15:50:48 +0200

    Trying to deploy.

--- deploy.bat
+++
@@ -1,7 +0,0 @@
-npm run build
-cd dist
-git init
-git add -A
-git commit -m'deploy'
-git push -f
-git push -f git@github.com:LuckyL00ser/vue-weather-app.git master:gh-pages
--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,5 @@ module.exports = {
   productionSourceMap: false,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-weather-app/'
-    : '/'
+    : './'
   }
-
-  // zwykly slash nie
-  //. CORS
-  ///'/vue-weather-app/'
